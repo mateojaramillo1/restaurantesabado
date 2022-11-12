b8c17d6a5811d44a391e3474403275dd44a79f49
afalarco22 <afalarco@unal.edu.co>
2022-11-12 12:48:10 -0500

    se crea la vista de datos de lo que se guardo en el formulario

--- config/config/urls.py
+++ config/config/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from web.views import Home,Platos,Empleados
+from web.views import Home,PlatosVista,Empleados
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', Home, name="Home"),
-    path('platos/', Platos, name="Platos"),
+    path('platos/', PlatosVista, name="Platos"),
     path('empleados/', Empleados, name="Empleados")
 ]
--- config/web/views.py
+++ config/web/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 from web.formularios.formularioPlatos import FormularioPlatos
 from web.formularios.formularioEmpleado import FormularioEmpleado
 
+from web.models import Platos
+
 
 # Create your views here.
 
@@ -11,7 +13,14 @@ from web.formularios.formularioEmpleado import FormularioEmpleado
 def Home(request):
     return render(request,'home.html')
 
-def Platos(request):
+def PlatosVista(request):
+    
+    
+    
+    #Rutina para consulta de platos
+    
+    platosConsultados=Platos.objects.all()
+    print(platosConsultados)
     
     #esta vista va a utilizar un formulario de django 
     #debo craer entonces un objeto de la clase formularioplatps
@@ -20,15 +29,42 @@ def Platos(request):
     
     #creamos un diccionario para enviar el formulario al html(template)
     data={
-        'formulario':formulario
+        'formulario':formulario,
+        'bandera': False,
+        'platos': platosConsultados
     }
 
     #RECIBIMOS LOS DATOS DEL FORMULARIO
-    if request.method == "POST":
-        datosFormulario=FormularioPlatos(request.POST)
-        if datosFormulario.is_valid():
-            datosLimpios=datosFormulario.cleaned_data
-            print(datosLimpios)
+    if request.method=='POST':
+        #deberiamos capturar los datos del formulario
+        datosDelFormulario=FormularioPlatos(request.POST)
+        #verificar si los datos llegaron correctamente(VALIDACIONES OK)
+        if datosDelFormulario.is_valid():
+            #capturamos la data
+            datosPlato=datosDelFormulario.cleaned_data
+            #creamos un objeto del tipo MODELO PLATO
+            platoNuevo=Platos(
+                nombre=datosPlato["nombre"],
+                descripcion=datosPlato["descripcion"],
+                fotografia=datosPlato["fotografia"],
+                precio=datosPlato["precio"],
+                tipo=datosPlato["tipo"]
+            )
+            #Intentamos llevar el objeto platoNuevo a LA BD
+            try:
+                platoNuevo.save()
+                data["bandera"]=True
+                print("EXITO GUARDANDO LOS DATOS")
+            
+            except Exception as error:
+                print("error",error)
+                data["bandera"]=False
+
+            
+            #try:
+               # platoNuevo.save()
+                #data["bandera"]=True
+            #ca
         
     
     return render(request,'menuplatos.html', data)
